676ea4d7eed6501793d82f267354433042ad4134
wangtianliang <wtl198937@163.com>
2022-05-17 09:25:44 +0800

    增加vercel支持

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const express = require('express')
 const path = require("path")
 var bodyParser = require('body-parser')
-const webpush = require('web-push');
 const cors = require("cors")
+const notify = require("./notify")
 //webpush.generateVAPIDKeys() 
-const vapidkey = {
-    publicKey: 'BPSTO906RQvft9aGXxTE15CLji_xEVN2bCjYnk5bcr5cPm0EaDOs1NMlRzNVn9NZxHY2KIzpvx0bpPfAnP8ale8',
-    privateKey:'fER5Cn-VypUYwjLSmAzRFtMK3NpHK0bHr1hEkhVdG4I'
-}
-var subscriptionList =[]
+var myNotify = new notify()
 const port = 8081
 const app = express();
 
@@ -28,55 +24,15 @@ app.listen(port, () => {
 
 
 app.post('/register', (req, res) => {
-
-    for (sub of subscriptionList) {
-        if (sub.endpoint == req.body.subscription.endpoint) {
-            return res.sendStatus(201)
-        }
-    }
-    subscriptionList.push(req.body.subscription)
-    console.log("register web",JSON.stringify(req.body.subscription))
+    myNotify.register(req.body)
     res.sendStatus(201)
 });
 app.post('/unregister', (req, res) => {
     console.log("unregister",req.body)
-    for (let i = 0; i < subscriptionList.length;++i ) {
-        if (subscriptionList[i].endpoint == req.body.subscription.endpoint) {
-            console.log("delete ",req.body.subscription.endpoint)
-            subscriptionList.splice(i,1)
-        }
-    }
+    myNotify.unregister(req.body)
     res.sendStatus(201)
 });
 app.post("/pushMsg", (req, res) => {
-    console.log("push message", req.body)
-    webpush.setGCMAPIKey('AAAAE7EM338:APA91bHNziRBD3n5bAuxb2HdlQEco24sTiG3nGn4PJKfg1t4ObCcG2P3wAZAMBPscInAJGH1HMDUlESR_Ye4RYE61IEh9sV5O7aSZtV8gbxIRdHfFcf8KosiqNzixEkG2mBwZvn6gbg7')
-    // 设置 VAPID
-    webpush.setVapidDetails(
-        'https://my-site.com/contact',
-        vapidkey.publicKey,
-        vapidkey.privateKey
-    );
-    for (let subscription of subscriptionList) {
-        console.log("subscript", JSON.stringify(subscription))
-        // 消息推送
-        webpush
-            .sendNotification(
-                subscription,
-                JSON.stringify({
-                    msg: req.body.message,
-                    url: "www.baidu.com",
-                    icon:""
-                })
-                
-            )
-            .then(result => {
-                console.log("push success")
-                res.sendStatus(201)
-            } )
-            .catch(err => console.log("push err",err));
-    }
-   
-    
-   
+    myNotify.pushMsg(req.body)
+    res.sendStatus(201)
 })
---
+++ notify.js
@@ -0,0 +1,62 @@
+const webpush = require('web-push');
+const vapidkey = {
+    publicKey: 'BPSTO906RQvft9aGXxTE15CLji_xEVN2bCjYnk5bcr5cPm0EaDOs1NMlRzNVn9NZxHY2KIzpvx0bpPfAnP8ale8',
+    privateKey:'fER5Cn-VypUYwjLSmAzRFtMK3NpHK0bHr1hEkhVdG4I'
+}
+class notify {
+    constructor() {
+        this.subscriptionList = []
+    }
+
+    register(body) {
+        for (sub of this.subscriptionList) {
+            if (sub.endpoint == body.subscription.endpoint) {
+                return true
+            }
+        }
+        this.subscriptionList.push(body.subscription)
+        console.log("register web", JSON.stringify(body.subscription))
+        return true
+    }
+    unregister(body) {
+        for (let i = 0; i < this.subscriptionList.length; ++i) {
+            if (this.subscriptionList[i].endpoint == body.subscription.endpoint) {
+                console.log("delete ", body.subscription.endpoint)
+                this.subscriptionList.splice(i, 1)
+            }
+        }
+        return true
+    }
+    pushMsg(body) {
+        console.log("push message", body)
+        webpush.setGCMAPIKey('AAAAE7EM338:APA91bHNziRBD3n5bAuxb2HdlQEco24sTiG3nGn4PJKfg1t4ObCcG2P3wAZAMBPscInAJGH1HMDUlESR_Ye4RYE61IEh9sV5O7aSZtV8gbxIRdHfFcf8KosiqNzixEkG2mBwZvn6gbg7')
+        // 设置 VAPID
+        webpush.setVapidDetails(
+            'https://my-site.com/contact',
+            vapidkey.publicKey,
+            vapidkey.privateKey
+        );
+        for (let subscription of this.subscriptionList) {
+            console.log("subscript", JSON.stringify(subscription))
+            // 消息推送
+            webpush
+                .sendNotification(
+                    subscription,
+                    JSON.stringify({
+                        msg: body.message,
+                        url: "www.baidu.com",
+                        icon: ""
+                    })
+
+                )
+                .then(result => {
+                    console.log("push success")
+                })
+                .catch(err => { 
+                    console.log("push err", err)
+                });
+        }
+    }
+}
+
+module.exports = notify
